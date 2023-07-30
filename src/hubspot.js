72ace957436b1b7863859a796a617e1795905e48
fabriciosilvaJr <fabriciosilva187@gmail.com>
2023-07-30 19:19:06 -0300

    Correção na Adição de contatos

--- src/hubspot.js
+++ src/hubspot.js
@@ -1,6 +1,10 @@
 async function createContacts(hubspotClient, contacts) {
   for (let i = 0; i < contacts.length; i++) {
-    await hubspotClient.crm.contacts.basicApi.create(contacts[i])
+    try {
+      await hubspotClient.crm.contacts.basicApi.create(contacts[i])
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
